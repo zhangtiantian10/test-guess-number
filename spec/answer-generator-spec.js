5714f266845f21545c697ef02c18cd5839b7f7e2
zhangtiantian10 <975571814@qq.com>
2016-07-27 12:46:07 +0800

    modify AnswerGenerator.randomNumber to AnswerGenerator.getAnswer method

--- spec/answer-generator-spec.js
+++ spec/answer-generator-spec.js
@@ -2,11 +2,11 @@ const AnswerGenerator = require('../src/models/answer-generator');
 
 describe('AnswerGenerator', () => {
     it('should return random answer', () => {
-        expect(AnswerGenerator.randomNumber().length).toEqual(4);
+        expect(AnswerGenerator.getAnswer().length).toEqual(4);
 
-        expect(AnswerGenerator.randomNumber()).not.toEqual(AnswerGenerator.randomNumber());
+        expect(AnswerGenerator.getAnswer()).not.toEqual(AnswerGenerator.getAnswer());
 
-        const answers = AnswerGenerator.randomNumber().split('');
+        const answers = AnswerGenerator.getAnswer().split('');
         for(const answer of answers){
             expect(answers.indexOf(answer)).toEqual(answers.lastIndexOf(answer));
         }
